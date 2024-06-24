395b6f35af5e55d89f9a950c13d2d79d76105bbc
anthonymartin36 <anthony.martin36@gmail.com>
2024-06-24 17:07:47 +1200

    changes in images url

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -6,21 +6,14 @@ const __dirname = Path.dirname(__filename)
 
 export default {
   development: {
-    client: 'pg',
+    client: 'sqlite3',
     useNullAsDefault: true,
-    connection:  "postgres://user:B1PdOTrnueaKEmUQhaDfJ8HkzFfOJRPd@dpg-confcs0cmk4c73a482b0-a.singapore-postgres.render.com/missingpurrson?ssl=true", //process.env.DATABASE_URL,
-    migrations: {
-      directory: "./migrations", 
-      schemaName: 'public',
+    connection: {
+      filename: Path.join(__dirname, 'dev.sqlite3'),
     },
-    // client: 'sqlite3',
-    // useNullAsDefault: true,
-    // connection: {
-    //   filename: Path.join(__dirname, 'dev.sqlite3'),
-    // },
-    // pool: {
-    //   afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
-    // },
+    pool: {
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+    },
   },
 
   test: {
