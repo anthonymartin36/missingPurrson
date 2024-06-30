37586cd0f101109e2d15d44df6e16de293414afc
anthonymartin36 <anthony.martin36@gmail.com>
2024-07-01 10:36:40 +1200

    working version of client side

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -39,8 +39,6 @@ export default function Nav({
     log.loginWithRedirect() //{ redirect_uri: 'http://localhost:5173/registeruser' }
     navigate('/registeruser')
 
-    //console.log('component ', currentUser)
-    //UseQuery to establish if user exists
   }
   return (
     <header>
@@ -65,7 +63,19 @@ export default function Nav({
           </div>
           <div style={navBorderStyle} className="nav-div"></div>
           <div className="nav__links">
-            <svg
+            <IfAuthenticated>
+              <Link to="/" onClick={handleSignOut}>
+                Sign out
+              </Link>
+              {userLogged && ( //{userLogged?.nickname}{' '}
+                <p>
+                  <img src={userLogged?.picture} alt={userLogged?.nickname} />
+                </p>
+              )}
+            </IfAuthenticated>
+            <IfNotAuthenticated>
+              <Link to="/" onClick={handleSignIn}>
+              <svg
               xmlns="http://www.w3.org/2000/svg"
               height="16"
               width="14"
@@ -73,19 +83,6 @@ export default function Nav({
             >
               <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
             </svg>
-            <IfAuthenticated>
-              <Link to="/" onClick={handleSignOut}>
-                Sign out
-              </Link>
-              {userLogged && (
-                <p>
-                  Signed in as: {userLogged?.nickname}{' '}
-                  <img src={userLogged?.picture} alt={userLogged?.nickname} />
-                </p>
-              )}
-            </IfAuthenticated>
-            <IfNotAuthenticated>
-              <Link to="/" onClick={handleSignIn}>
                 Sign in
               </Link>
             </IfNotAuthenticated>
--- client/index.tsx
+++ client/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { Auth0Provider } from '@auth0/auth0-react'
+import { Auth0Provider  } from '@auth0/auth0-react'
 import { RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
@@ -13,11 +13,14 @@ const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <Auth0Provider
-      domain="https://dev-1ackkh7witjzru1x.us.auth0.com" 
-      clientId="ipcjUqS4hMyxTLvNhK4gnncdtYJQG78d"
-      redirectUri={window.location.origin}
-      audience="https://missingPurrson/api"
+    <Auth0Provider 
+    authorizationParams={{ 
+      redirectUri: window.location.origin,
+      audience: import.meta.env.VITE_APP_AUTH0_AUDIENCE
+    }}
+    domain={import.meta.env.VITE_APP_AUTH0_DOMAIN} 
+    clientId={import.meta.env.VITE_APP_AUTH0_CLIENT_ID}
+    
     >
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
@@ -27,3 +30,6 @@ document.addEventListener('DOMContentLoaded', () => {
     </Auth0Provider>,
   )
 })
+
+//redirectUri={window.location.origin}
+//audience={import.meta.env.VITE_APP_AUTH0_AUDIENCE}
